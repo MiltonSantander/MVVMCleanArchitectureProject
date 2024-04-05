836c394e9c2f5b0e93ddb2022f2f15d6a23e25ac
MiltonGarcia <milgarsan@gmail.com>
2024-04-05 09:42:37 -0400

    The corresponding libraries and configurations were added to the build.gradle (app)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,44 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    dataBinding { enable = true }
 }
 
 dependencies {
-
+    val lifecycleVersion = "2.7.0"
+    val roomVersion = "2.6.1"
+    val coroutineVersion = "1.8.1-Beta"
+    val daggerVersion = "2.51.1"
+    val retrofitVersion = "2.10.0"
+    val interceptorVersion = "4.12.0"
+    val glideVersion = "4.16.0"
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    // Saved state module for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVersion")
+    // Annotation processor
+    implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion")
+    //room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    //optional - Kotlin extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$roomVersion")
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+    //Dagger
+    implementation("com.google.dagger:dagger:$daggerVersion")
+    annotationProcessor("com.google.dagger:dagger-compiler:$daggerVersion")
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    //okhttp
+    implementation("com.squareup.okhttp3:logging-interceptor:$interceptorVersion")
+    //Glide
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+    annotationProcessor("com.github.bumptech.glide:compiler:$glideVersion")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
