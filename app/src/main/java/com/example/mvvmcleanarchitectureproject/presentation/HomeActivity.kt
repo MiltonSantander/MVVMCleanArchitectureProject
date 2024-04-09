963688fc7863fe370480c71ac25dfc265768d89e
MiltonGarcia <milgarsan@gmail.com>
2024-04-09 01:19:19 -0400

    The data Binding is implemented in the HomeActivity and the setOnClickListener of the buttons was configured through it

--- app/src/main/java/com/example/mvvmcleanarchitectureproject/presentation/HomeActivity.kt
+++ app/src/main/java/com/example/mvvmcleanarchitectureproject/presentation/HomeActivity.kt
@@ -1,12 +1,30 @@
 package com.example.mvvmcleanarchitectureproject.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.mvvmcleanarchitectureproject.R
+import com.example.mvvmcleanarchitectureproject.databinding.ActivityHomeBinding
+import com.example.mvvmcleanarchitectureproject.presentation.artist.ArtistActivity
+import com.example.mvvmcleanarchitectureproject.presentation.movie.MovieActivity
+import com.example.mvvmcleanarchitectureproject.presentation.tvshow.TvShowActivity
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        binding.movieButton.setOnClickListener {
+            startActivity(Intent(this, MovieActivity::class.java))
+        }
+
+        binding.tvButton.setOnClickListener {
+            startActivity(Intent(this, TvShowActivity::class.java))
+        }
+
+        binding.artistButton.setOnClickListener {
+            startActivity(Intent(this, ArtistActivity::class.java))
+        }
     }
 }
