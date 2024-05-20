d68032ef7d9679f8b9e2349b65fcc37f5c924196
MiltonGarcia <milgarsan@gmail.com>
2024-05-19 20:12:22 -0400

    In the MovieActivity the call to the service was implemented to obtain the list of movies, and test the injection of dependencies

--- app/src/main/java/com/example/mvvmcleanarchitectureproject/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/example/mvvmcleanarchitectureproject/presentation/movie/MovieActivity.kt
@@ -2,14 +2,31 @@ package com.example.mvvmcleanarchitectureproject.presentation.movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmcleanarchitectureproject.R
+import com.example.mvvmcleanarchitectureproject.data.model.movie.Movie
 import com.example.mvvmcleanarchitectureproject.databinding.ActivityMovieBinding
+import com.example.mvvmcleanarchitectureproject.presentation.di.Injector
+import javax.inject.Inject
 
 class MovieActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: MovieViewModelFactory
+    private lateinit var movieViewModel: MovieViewModel
     private lateinit var binding: ActivityMovieBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
+        (application as Injector).createMovieSubComponent()
+            .inject(this)
+        movieViewModel = ViewModelProvider(this, factory)[MovieViewModel::class.java]
+        val responseLiveData: LiveData<List<Movie>?> = movieViewModel.getMovieList()
+        responseLiveData.observe(this) {
+            Log.i("mytag", it.toString())
+        }
     }
 }
